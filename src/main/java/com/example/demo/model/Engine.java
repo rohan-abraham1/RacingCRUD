24d64182e65613eaf90cd6f9bf3e3f7ef75d2caf
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-09-07 10:15:33 +0530

    Completed CRUD

--- src/main/java/com/example/demo/model/Engine.java
+++ src/main/java/com/example/demo/model/Engine.java
@@ -33,10 +33,10 @@ public class Engine {
 		this.hqLocation = hqLocation;
 	}
 	
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getName() {
